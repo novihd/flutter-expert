5a5c969814a554f5f5ef578e48562e76174fb2c8
novihd <novihd@aol.com>
2022-11-27 20:29:33 +0700

    test

---
+++ test/dummy_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Plus Operator', () {
+    test('Plus operation two number', () {
+      expect(1 + 1, 2);
+    });
+  });
+}
